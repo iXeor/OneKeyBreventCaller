e0ca7abb6b056641047bc0e958e80e8ad58f3d37
Akatsuki <43687278+iXeor@users.noreply.github.com>
2021-04-09 15:56:20 +0800

    Add files via upload

---
+++ OneKeyBreventCaller/Caller.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace OneKeyBreventCaller
+{
+    partial class BreventCallerMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BreventCallerMain));
+            this.Run = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DeviceInfoStr = new System.Windows.Forms.Label();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Run
+            // 
+            this.Run.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Run.Location = new System.Drawing.Point(410, 92);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(75, 23);
+            this.Run.TabIndex = 0;
+            this.Run.Text = "激活";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "设备型号：";
+            // 
+            // DeviceInfoStr
+            // 
+            this.DeviceInfoStr.AutoSize = true;
+            this.DeviceInfoStr.Location = new System.Drawing.Point(119, 43);
+            this.DeviceInfoStr.Name = "DeviceInfoStr";
+            this.DeviceInfoStr.Size = new System.Drawing.Size(84, 17);
+            this.DeviceInfoStr.TabIndex = 2;
+            this.DeviceInfoStr.Text = "DeviceModel";
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Location = new System.Drawing.Point(329, 92);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(75, 23);
+            this.RefreshButton.TabIndex = 5;
+            this.RefreshButton.Text = "刷新";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.Refreshbutton_Click);
+            // 
+            // BreventCallerMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 127);
+            this.Controls.Add(this.RefreshButton);
+            this.Controls.Add(this.DeviceInfoStr);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Run);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BreventCallerMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "一键激活黑阈 - OneKey Brevent Caller";
+            this.Load += new System.EventHandler(this.BreventCallerMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button Run;
+        private Label label1;
+        private Label DeviceInfoStr;
+        private Button RefreshButton;
+    }
+}
+
---
+++ OneKeyBreventCaller/Caller.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace OneKeyBreventCaller
+{
+    public partial class BreventCallerMain : Form
+    {
+        private int count;
+        private Process p = new Process();
+
+        private void ProcessInit()
+        {
+            p.StartInfo.FileName = "./ADB_Bin/adb.exe";//设定程序名
+            p.StartInfo.UseShellExecute = false; //关闭shell的使用
+            p.StartInfo.RedirectStandardInput = true; //重定向标准输入
+            p.StartInfo.RedirectStandardOutput = true; //重定向标准输出
+            p.StartInfo.RedirectStandardError = true; //重定向错误输出
+            p.StartInfo.CreateNoWindow = true;//设置不显示窗口
+        }
+
+        private void DeviceScanner()
+        {
+            ProcessInit();
+            p.StartInfo.Arguments = " kill-server";
+            p.Start();
+            p.Close();
+            p.StartInfo.Arguments = " shell getprop ro.product.model";
+            p.Start();
+            DeviceInfoStr.Text = p.StandardOutput.ReadToEnd();
+            p.Close();
+            if (DeviceInfoStr.Text == "")
+            {
+                if (count <= 5)
+                {
+                    DialogResult result = MessageBox.Show(@"请确保手机已连接并开启USB调试！", @"提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                    if (result == DialogResult.OK)
+                    {
+                        DeviceScanner();
+                    }
+                    else if (result == DialogResult.Cancel)
+                    {
+                        DeviceInfoStr.Text = @"（暂无）";
+                    }
+                }
+            }
+        }
+
+        public BreventCallerMain()
+        {
+            InitializeComponent();
+        }
+        private void BreventCallerMain_Load(object sender, EventArgs e)
+        {
+            DeviceScanner();
+        }
+        private void Refreshbutton_Click(object sender, EventArgs e)
+        {
+            DeviceScanner();
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            ProcessInit();
+            p.StartInfo.Arguments = " -d shell sh /data/data/me.piebridge.brevent/brevent.sh";
+            p.Start();
+            string msg = p.StandardOutput.ReadToEnd();
+            p.Close();
+            if(msg != "")
+            {
+                DialogResult res = MessageBox.Show("黑阈启动成功！\n"+msg, @"恭喜", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (res == DialogResult.OK)
+                {
+                    Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show(@"黑阈启动失败，请确保手机已连接并开启USB调试！", @"提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
